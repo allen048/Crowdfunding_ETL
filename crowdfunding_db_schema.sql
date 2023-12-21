b82c83fee27d5ebfd397640234e44fd9ceb4162c
Qasim Khilji <46390226+mqkhilji@users.noreply.github.com>
2023-12-21 09:22:04 -0700

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -11,20 +11,26 @@ email varchar(255)
 Category
 -
 category_id int PK
-category varchar(255)
+category varchar(255) FK >- Campaign.category
 
 Subcategory
 -
 subcategory_id int PK
-subcategory varchar(255)
+subcategory varchar(255) FK >- Campaign.subcategory
 
 Campaign
 -
 cf_id int PK
 contact_id int FK >- Contacts.contact_id
-staff_pick varchar(255)
-spotlight varchar(255)
+company_name  varchar(255)
+description TEXT
+goal INT
+pledged INT
+outcome INT
+backers_count INT
+country INT
+currency INT
+launch_date DATE
+end_date DATE
 category varchar(255)
 subcategory varchar(255)
-category_id int FK >- Category.category_id
-subcategory_id int FK >- Subcategory.subcategory_id
